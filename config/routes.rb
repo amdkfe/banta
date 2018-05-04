d47790d47bdaf16affe5d6b74fc36ba3866d5247
Ahmad Kafaei <amdkfe@gmail.com>
2018-05-04 13:46:04 +0100

    implemented authentication and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_up: 'register', sign_out:'logout'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
   resources :rooms
+  root to: "rooms#index"
 end
 
 
- # root to: "messages#index"
 
